7bc9acca315b38d46f99d4c3fa8cb51ce284de0c
Muhammad Haris <isharis@gmail.com>
2018-04-11 15:54:51 +0500

    Setting up Redux Frameowrk

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Plugin URI:
  * Description: mightyblocks — is a Gutenberg plugin created via create-guten-block.
  * Author: mharis
- * Author URI: 
+ * Author URI:
  * Version: 1.0.0
  * License: GPL2+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
@@ -21,3 +21,10 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Block Initializer.
  */
 require_once plugin_dir_path( __FILE__ ) . 'src/init.php';
+
+if ( ! class_exists( 'ReduxFramework' ) && file_exists( dirname( __FILE__ ) . '/libs/ReduxFramework/ReduxCore/framework.php' ) ) {
+    require_once( dirname( __FILE__ ) . '/libs/ReduxFramework/ReduxCore/framework.php' );
+}
+if ( ! isset( $redux_demo ) && file_exists( dirname( __FILE__ ) . '/libs/ReduxFramework/sample/sample-config.php' ) ) {
+    require_once( dirname( __FILE__ ) . '/libs/ReduxFramework/sample/sample-config.php' );
+}
